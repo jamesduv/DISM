49aaf3b6f0c976a982a11d371fc22f3cf4dfaa73
James <jamesduv0012@gmail.com>
2022-02-20 23:03:03 -0500

    initial commit

---
+++ dense_networks.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import tensorflow as tf
+import numpy as np
+import os
+import losses
+
+
+def swish(x):
+    return (x*tf.keras.activations.sigmoid(x))
+
+tf.keras.utils.get_custom_objects().update({'swish': swish})   
+tf.keras.utils.get_custom_objects().update({'swish_fn': swish})
+
+class dense_base():
+    def __init__(self, opt, data=None):
+        self.opt    = opt
+        self.data   = data
+
+    def set_kernel_regularizer(self):
+        '''Set the kernel regularizer using options defined in self.opt'''
+
+        regularizers = {'none'  : tf.keras.regularizers.l2( l= 0),
+                        'l1'    : tf.keras.regularizers.l1( l=self.opt['lambda_l1']),
+                        'l2'    : tf.keras.regularizers.l2( l=self.opt['lambda_l2']),
+                        'l1_l2' : tf.keras.regularizers.l1_l2( l1=self.opt['lambda_l1'],
+                                                            l2=self.opt['lambda_l2']),
+                        None    : None}
+        if self.opt['kernel_regularizer'] not in regularizers.keys():
+                raise Exception('Unsupported kernel regularizer specified: {}'.format(self.opt['kernel_regularizer']))
+        self.kernel_regularizer = regularizers[self.opt['kernel_regularizer']]
+
+    def set_activation(self):
+        '''Set the activation function using options defined in self.opt'''
+
+        activations = {'elu':tf.keras.activations.elu,
+                       'hard_sigmoid':tf.keras.activations.hard_sigmoid,
+                       'linear':tf.keras.activations.linear,
+                       'relu':tf.keras.activations.relu,
+                       'selu':tf.keras.activations.selu,
+                       'sigmoid':tf.keras.activations.sigmoid,
+                       'softmax':tf.keras.activations.softmax,
+                       'tanh':tf.keras.activations.tanh,
+                       'swish':swish}
+        if self.opt['activation'] not in activations.keys():
+                raise Exception('Unsupported activation function specified: {}'.format(self.opt['activation']))
+        self.activation = activations[self.opt['activation']]
+
+    def set_optimizer(self):
+        '''Set the activation function using options defined in self.opt'''
+
+        optimizers = {'Adadelta':tf.keras.optimizers.Adadelta,
+                      'Adagrad':tf.keras.optimizers.Adagrad,
+                      'Adam':tf.keras.optimizers.Adam,
+                      'Adamax':tf.keras.optimizers.Adamax,
+                      'Ftrl':tf.keras.optimizers.Ftrl,
+                      'Nadam':tf.keras.optimizers.Nadam,
+                      'RMSprop':tf.keras.optimizers.RMSprop,
+                      'SGD':tf.keras.optimizers.SGD}
+        if self.opt['optimizer'] not in optimizers.keys():
+            raise Exception('Unsupported optimizer specified: {}'.format(self.opt['optimizer']))
+        self.optimizer = optimizers[self.opt['optimizer']](learning_rate = self.opt['learning_rate'], **self.opt['optimizer_kwargs'])
+
+    def set_loss(self):
+        '''Set the loss function using options defined in self.opt'''
+
+        all_losses = {'KLD':tf.keras.losses.KLD,
+                  'MAE':tf.keras.losses.MAE,
+                  'MAPE':tf.keras.losses.MAPE,
+                  'MSE':tf.keras.losses.MSE,
+                  'mse':tf.keras.losses.MSE,
+                  'MSLE':tf.keras.losses.MSLE,
+                  'binary_crossentropy':tf.keras.losses.binary_crossentropy,
+                  'categorical_crossentropy':tf.keras.losses.categorical_crossentropy,
+                  'categorical_hinge':tf.keras.losses.categorical_hinge,
+                  'mse_gs':None}
+
+        if self.opt['loss'] not in all_losses.keys():
+            raise Exception('Unsupported loss function specified: {}'.format(self.opt['loss']))
+        if self.opt['loss'] == 'mse_gs':
+            self.loss = losses.mse_grad_sharp(lambda_gs = self.opt['lambda_gs'])
+        else:
+            self.loss = all_losses[self.opt['loss']]
+
+class dense_v1(dense_base):
+    def __init__(self, opt, data=None):
+        dense_base.__init__(self, opt, data)
+
+        #set items required to build model
+        # self.set_kernel_regularizer()
+        self.set_activation()
+        self.call_backs = None
+
+
+    def build_model(self):
+        self.layer_names = []
+
+        #input layer
+        input1 = tf.keras.Input(shape=(self.opt['input_dim']), name='input')
+        output = None
+
+
+        # Dense Layers Construction
+        print('Constructing Dense Layers')
+        for iDense in range(self.opt['n_layers']):
+            layername = 'dense_{:1.0f}'.format(iDense)
+            print(layername)
+            self.layer_names.append(layername)
+            units = self.opt['n_nodes'][iDense]
+            activation = self.activation
+
+            if iDense == 0:
+                output = tf.keras.layers.Dense(units   = units,
+                                            activation = activation,
+                                            name       = layername)(input1)
+            else:
+                if iDense == (self.opt['n_layers']-1):
+                    units = self.opt['n_output']
+                    if self.opt['is_linear_output']:
+                        activation = tf.keras.activations.linear
+                    else:
+                        activation = self.activation
+                    
+                output = tf.keras.layers.Dense(units         = units,
+                                                activation   = activation,
+                                                name         = layername)(output)
+
+        self.model = tf.keras.Model(inputs=[input1], outputs=output)
+        self.model.summary()
+
+    def train_model_keras(self):
+        self.model.compile(optimizer = self.optimizer, loss=self.loss)
+        self.history = self.model.fit(x         = self.data['x_train'],
+                                      y         = self.data['y_train'],
+                                      epochs    = self.opt['epochs'],
+                                      callbacks = self.call_backs,
+                                      batch_size = self.opt['batch_size'],
+                                      validation_data = (self.data['x_val'], self.data['y_val']))
+
+    def train_model_keras_noval(self):
+        self.model.compile(optimizer = self.optimizer, loss=self.loss)
+        self.history = self.model.fit(x         = self.data['x_train'],
+                                      y         = self.data['y_train'],
+                                      epochs    = self.opt['epochs'],
+                                      callbacks = self.call_backs,
+                                      batch_size = self.opt['batch_size'])
+
+    def set_save_paths(self):
+        self.fn_csv             = os.path.join(self.opt['save_dir'], 'training.csv')
+
+        self.fn_weights_val_best    = os.path.join(self.opt['save_dir'], 'weights.val_best.h5')
+        self.fn_weights_train_best  = os.path.join(self.opt['save_dir'], 'weights.train_best.h5')
+        self.fn_weights_end         = os.path.join(self.opt['save_dir'], 'weights.end.h5')
+
+        self.fn_model_val_best      = os.path.join(self.opt['save_dir'], 'model.val_best.tf')
+        self.fn_model_train_best    = os.path.join(self.opt['save_dir'], 'model.train_best.tf')
+        self.fn_model_end           = os.path.join(self.opt['save_dir'], 'model.end.tf')
+
+    def make_callbacks_weights(self):
+        '''Make checkpoints to save the weights only during training'''
+        if self.call_backs is None:
+            self.call_backs = []
+
+        checkpoint = tf.keras.callbacks.ModelCheckpoint(self.fn_weights_val_best, 
+                                     monitor = 'val_loss',
+                                     verbose = 1,
+                                     save_best_only = True,
+                                     mode = 'min',
+                                     save_weights_only=True)
+        self.call_backs.append(checkpoint)
+        checkpoint_2 = tf.keras.callbacks.ModelCheckpoint(self.fn_weights_end,
+                                                        monitor='val_loss',
+                                                        verbose=1, 
+                                                        save_best_only=False,
+                                                        save_weights_only=True)
+        self.call_backs.append(checkpoint_2)
+
+        checkpoint_3 = tf.keras.callbacks.ModelCheckpoint(self.fn_weights_train_best, 
+                                     monitor = 'loss',
+                                     verbose = 1,
+                                     save_best_only = True,
+                                     mode = 'min',
+                                     save_weights_only=True)
+        self.call_backs.append(checkpoint_3)
+
+    def set_training_options(self):
+        self.set_optimizer()
+        self.set_loss()
+        self.set_save_paths()
+        self.make_callbacks_weights()
+        
+    def start_csv_logger(self):
+        csv_logger = tf.keras.callbacks.CSVLogger(self.fn_csv)
+        if self.call_backs is None:
+            self.call_backs = []
+        self.call_backs.append(csv_logger)
+    
+    def tensorboard_callbacks(self, histogram_freq=10, profile_batch=(1,5)):
+
+        self.log_dir = os.path.join(self.opt['save_dir'], 'tb_logs', 'training')
+        tb_callback  = tf.keras.callbacks.TensorBoard(log_dir = self.log_dir,
+                                                     histogram_freq = histogram_freq,
+                                                     write_graph    = True,
+                                                     profile_batch  = profile_batch )
+        if self.call_backs is None:
+            self.call_backs = []
+        self.call_backs.append(tb_callback)
+
+def set_network_func(opt):
+    allowed = {'dense_v1'   : dense_v1}
+    netfun  = allowed[opt['network_func']]
+    return netfun
+
+def get_dense_opt(network_func  = 'dense_v1',
+                  name          = 'dense_test_6',
+                  n_layers      = 6,  # number of layers, includes output layer
+                  n_nodes       = [50, 50, 50, 50, 50, 1],
+                  activation    = 'swish',
+                  data_opt      = {},
+                  inputs        =  ['xc', 'yc', 'sdf_overall'],
+                  input_dim     = 3,
+                  n_output      = 1,
+                  outputs       = ['q'],
+                  norm_x_by     = 'range',
+                  norm_y_by     = 'range',
+                  is_linear_output = True,
+                  is_data_batch     = False,
+                  n_data_batches    = 4,
+                  batch_size    = 1500,
+                  training_fraction = 0.8,
+                  epochs        = 5000,
+                  loss          = 'mse',
+                  optimizer     = 'Adam',
+                  n_epochs_save     = 25,
+                  learning_rate     = 1e-04,
+                  is_learning_rate_decay = True,
+                  kernel_regularizer = None,
+                  is_load_weights   = False,
+                  fn_weights_load   = None,   #weights to use IF continuing
+                  is_save_all_weights = True,
+                  lambda_l1         = 1e-05,
+                  lambda_l2         = 0,
+                  save_dir_base     = '../Dense_models/',
+                  is_debugging      = False):
+
+    if is_learning_rate_decay:
+        optimizer_kwargs = {'decay':learning_rate / epochs}
+    else:
+        optimizer_kwargs = {}
+
+    opt = {'network_func'   : network_func,
+           'name'           : name,
+           'n_layers'       : n_layers,
+           'n_nodes'        : n_nodes,
+           'activation'     : activation,
+           'data_opt'       : data_opt,
+           'inputs'         : inputs,
+           'input_dim'      : input_dim,
+           'n_output'       : n_output,
+           'outputs'        : outputs,
+           'norm_x_by'      : norm_x_by,
+           'norm_y_by'      : norm_y_by,
+           'is_linear_output' : is_linear_output,
+           'is_data_batch'  : is_data_batch,
+           'batch_size'     : batch_size,
+           'n_data_batches' : n_data_batches,
+           'training_fraction'  : training_fraction,
+           'epochs'         : epochs,
+           'loss'           : loss,
+           'optimizer'      : optimizer,
+           'optimizer_kwargs' : optimizer_kwargs,
+           'n_epochs_save'  : n_epochs_save,
+           'learning_rate'  : learning_rate,
+           'is_learning_rate_decay' : is_learning_rate_decay,
+           'kernel_regularizer' : kernel_regularizer,
+           'is_load_weights'    : is_load_weights,
+           'fn_weights_load'    : fn_weights_load,
+           'is_save_all_weights' : is_save_all_weights,
+           'lambda_l1'      : lambda_l1,
+           'lambda_l2'      : lambda_l2,
+           'save_dir_base'  : save_dir_base,
+           'is_debugging'   : is_debugging
+           }
+    return opt
+
---
+++ nids_keras_networks.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import tensorflow as tf
+
+def swish(x):
+    return (x*tf.keras.activations.sigmoid(x))
+tf.keras.utils.get_custom_objects().update({'swish': swish})
+
+class keras_nids_v0(tf.keras.Model):
+    '''Implementation of NIDS network where each the spatial and parameter networks are 
+    tf.keras models, supplied to the class constructor'''
+
+    def __init__(self, opt, spatial_net, parameter_net):
+        super(keras_nids_v0, self).__init__()
+        self.opt            = opt
+        self.spatial_net    = spatial_net
+        self.parameter_net  = parameter_net
+        self.call_backs = None
+        self.split_sizes    = self.opt['split_sizes']
+        self.wt_mat_shape   = self.opt['wt_mat_shape']
+
+    def call(self,x):
+        return(self.call_model(x = x[0], mu = x[1]))
+
+    def call_model(self, x, mu, training=False):
+        '''Call the full NIDS model, return only the final result'''
+
+        param_net_out   = self.parameter_net(mu, training=training)
+        weight, bias    = tf.split(param_net_out, num_or_size_splits=self.split_sizes, axis=1)
+        weight          = tf.reshape(weight, self.wt_mat_shape)
+        hx              = self.spatial_net(x, training=training)
+        y_pred          = tf.einsum('ijk,ik->ij', weight, hx) + bias
+        return y_pred
+    
+    def call_model_modes(self, x, mu, training=False):
+        '''Call the full NIDS model, return final result, hx, and weight/bias'''
+        param_net_out   = self.parameter_net(mu, training=training)
+        weight, bias    = tf.split(param_net_out, num_or_size_splits=self.split_sizes, axis=1)
+        weight          = tf.reshape(weight, self.wt_mat_shape)
+        hx              = self.spatial_net(x, training=training)
+        y_pred          = tf.einsum('ijk,ik->ij', weight, hx) + bias
+        return y_pred, hx, weight, bias
+
+
+    def set_save_paths(self):
+        self.fn_csv             = os.path.join(self.opt['save_dir'], 'training.csv')
+
+        self.fn_weights_val_best    = os.path.join(self.opt['save_dir'], 'weights.val_best.h5')
+        self.fn_weights_train_best  = os.path.join(self.opt['save_dir'], 'weights.train_best.h5')
+        self.fn_weights_end         = os.path.join(self.opt['save_dir'], 'weights.end.h5')
+
+        self.fn_model_val_best      = os.path.join(self.opt['save_dir'], 'model.val_best.tf')
+        self.fn_model_train_best    = os.path.join(self.opt['save_dir'], 'model.train_best.tf')
+        self.fn_model_end           = os.path.join(self.opt['save_dir'], 'model.end.tf')
+
+    def start_csv_logger(self):
+        '''Start the csv_logger, OVERWRITES self.call_backs'''
+        csv_logger = tf.keras.callbacks.CSVLogger(self.fn_csv)
+        if self.call_backs is None:
+            self.call_backs = []
+        self.call_backs.append(csv_logger)
+          
+    def make_callbacks_weights(self):
+        '''Make checkpoints to save the weights only during training'''
+        if self.call_backs is None:
+            self.call_backs = []
+
+        checkpoint = tf.keras.callbacks.ModelCheckpoint(self.fn_weights_val_best, 
+                                     monitor = 'val_loss',
+                                     verbose = 1,
+                                     save_best_only = True,
+                                     mode = 'min',
+                                     save_weights_only=True)
+        self.call_backs.append(checkpoint)
+        checkpoint_2 = tf.keras.callbacks.ModelCheckpoint(self.fn_weights_end,
+                                                        monitor='val_loss',
+                                                        verbose=1, 
+                                                        save_best_only=False,
+                                                        save_weights_only=True)
+        self.call_backs.append(checkpoint_2)
+
+        checkpoint_3 = tf.keras.callbacks.ModelCheckpoint(self.fn_weights_train_best, 
+                                     monitor = 'loss',
+                                     verbose = 1,
+                                     save_best_only = True,
+                                     mode = 'min',
+                                     save_weights_only=True)
+        self.call_backs.append(checkpoint_3)
+
+    def make_callbacks_model(self):
+        '''Make callbacks to save the model to file'''
+        if self.call_backs is None:
+            self.call_backs = []
+        
+        checkpoint = tf.keras.callbacks.ModelCheckpoint(self.fn_model_val_best, 
+                                     monitor = 'val_loss',
+                                     verbose = 1,
+                                     save_best_only = True,
+                                     mode = 'min',
+                                     save_weights_only=False,
+                                     save_format='.tf')
+        self.call_backs.append(checkpoint)
+
+        checkpoint_2 = tf.keras.callbacks.ModelCheckpoint(self.fn_model_end,
+                                                          monitor='val_loss',
+                                       verbose=1, save_best_only=False,
+                                       save_weights_only=False,
+                                       save_format='.tf')
+        self.call_backs.append(checkpoint_2)
+
+        checkpoint_3 = tf.keras.callbacks.ModelCheckpoint(self.fn_model_train_best, 
+                                     monitor = 'loss',
+                                     verbose = 1,
+                                     save_best_only = True,
+                                     mode = 'min',
+                                     save_weights_only=False,
+                                     save_format='.tf')
+        self.call_backs.append(checkpoint_3)
+
+    def tensorboard_callbacks(self, histogram_freq=10, profile_batch=(1,5)):
+
+        self.log_dir = os.path.join(self.opt['save_dir'], 'tb_logs', 'training')
+        tb_callback  = tf.keras.callbacks.TensorBoard(log_dir = self.log_dir,
+                                                     histogram_freq = histogram_freq,
+                                                     write_graph    = True,
+                                                     profile_batch  = profile_batch )
+        if self.call_backs is None:
+            self.call_backs = []
+        self.call_backs.append(tb_callback)
+
+def get_keras_nids_v0_opt(network_func  = 'keras_nids_v0',
+                            name        = '',
+                            data_opt    = {},
+                            noutput    = 3,
+                            nspatial   = 3,
+                            nstate     = 3,
+                            nparam     = 4,
+                            hx_dim      = 50,
+                            wt_mat_dim  = '',
+                            wt_total_dim = '',
+                            split_sizes = [],
+                            wt_mat_shape = (),
+                            inputs      = ['xc', 'yc', 'sdf'],
+                            params      = [],
+                            outputs     = [],
+                            spatial_opt = None,
+                            param_opt   = None,
+                            activation  = 'swish',
+                            norm_x_by       = 'range',
+                            norm_mu_by      = 'range',
+                            norm_y_by       = 'range',
+                            batch_size      = 1500,
+                            learning_rate   = 1e-04,
+                            is_learning_rate_decay = True,
+                            training_fraction = 0.8,
+                            epochs          = 2000,
+                            loss            = 'mse',
+                            optimizer       = 'Adam',
+                            kernel_regularizer = None,
+                            lambda_l1       = 0,
+                            lambda_l2       = 0,
+                            save_dir_base   = '../NICE_Predictions',
+                            is_debugging    = False):
+    if is_learning_rate_decay:
+        optimizer_kwargs = {'decay':learning_rate / epochs}
+    else:
+        optimizer_kwargs = {}
+
+    
+
+    opt = { 'network_func'  : network_func,
+            'name'          : name,
+            'data_opt'      : data_opt,
+            'noutput'      : noutput,
+            'nspatial'     : nspatial,
+            'nstate'       : nstate,
+            'nparam'       : nparam,
+            'hx_dim'        : hx_dim,
+            'wt_mat_dim'    : wt_mat_dim,
+            'wt_total_dim'  : wt_total_dim,
+            'split_sizes'   : split_sizes,
+            'wt_mat_shape'  : wt_mat_shape,
+            'inputs'        : inputs,
+            'params'        : params,
+            'outputs'       : outputs,
+            'spatial_opt'   : spatial_opt,
+            'param_opt'     : param_opt,
+            'activation'    : activation,
+            'norm_x_by'     : norm_x_by,
+            'norm_mu_by'    : norm_mu_by,
+            'norm_y_by'     : norm_y_by,
+            'batch_size'    : batch_size,
+            'learning_rate' :learning_rate,
+            'is_learning_rate_decay' : is_learning_rate_decay,
+            'training_fraction' :training_fraction,
+            'epochs'        : epochs,
+            'loss'          : loss,
+            'optimizer'     : optimizer,
+            'optimizer_kwargs' : optimizer_kwargs,
+            'kernel_regularizer' : kernel_regularizer,
+            'lambda_l1'     : lambda_l1,
+            'lambda_l2'     : lambda_l2,
+            'save_dir_base' : save_dir_base,
+            'is_debugging'  : is_debugging}
+    return opt
+
+    
+
